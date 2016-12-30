1aae14ea310a1bdb2849feec5a92e1bf7f53d71a
Matthias Guenther <matthias@wikimatze.de>
2016-12-30 20:21:26 +0100

    Remove facebook js and track commits

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // for the latest github commits
 $(function() {
     $('#github-commits').githubInfoWidget(
-        { user: 'wikimatze', repo: 'padrinobook', branch: 'master', last: 5, limitMessageTo: 30 });
+        { user: 'padrinobook', repo: 'padrinobook', branch: 'master', last: 5, limitMessageTo: 30 });
 });
 
 
@@ -16,18 +16,6 @@ _paq.push(['enableLinkTracking']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
     g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
-
-
-// facebook tracking
-
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 
 // <script type="text/javascript">
 // $(function(){
